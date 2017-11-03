cb4927ab8b54a69c79cea685f05bacbae369f57e
lolxy <lolxy@users.noreply.github.com>
2017-11-03 11:34:04 +0800

    修改配置信息和新增错误模块
    修改配置信息和新增错误模块

--- config/prod.env.js
+++ config/prod.env.js
@@ -6,12 +6,14 @@ module.exports = {
   // corpsiteBaseUrl: '"//corpsite.fccn.cc"',
   // qiniuBucketHost: '"//files.fccn.cc"',
   // nimAppkey: '"8c41f31bb3cfadea54e527a140c86818"',
-  // downloadUrl:'"//api.fccn.cc/UpFiles/ExcelFiles/importExcel.xlsx"'
+  // downloadUrl:'"//api.fccn.cc/UpFiles/ExcelFiles/importExcel.xlsx"',
+  // baseUrl:'"//www.fccn.cc"'
   apiRoot: '"//devapi.fccn.cc/Api/v1.1"',
   apiMall: '"//marketapidev.fccn.cc/api/market"',
   apiCham: '"//marketapidev.fccn.cc/api/commerceChamber"',
   corpsiteBaseUrl: '"//corpsitedev.fccn.cc"',
   qiniuBucketHost: '"//filesdev.fccn.cc"',
   nimAppkey: '"9c4d8d4b4466d7c2397bf1741e59e013"',
-  downloadUrl:'"//devapi.fccn.cc/UpFiles/ExcelFiles/importExcel.xlsx"'
+  downloadUrl:'"//devapi.fccn.cc/UpFiles/ExcelFiles/importExcel.xlsx"',
+  baseUrl:'"//dev.fccn.cc"'
 };
